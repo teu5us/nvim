13950e6f5765c5518fa834900caec1e35279ed04
Pavel Stepanov <paulkreuzmann@gmail.com>
2025-01-01 22:59:17 +0600

    ivy picker for find_files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -52,6 +52,9 @@ return {
                 },
             },
             pickers = {
+                find_files = {
+                    theme = 'ivy',
+                },
                 live_grep = {
                     theme = 'ivy',
                 },
